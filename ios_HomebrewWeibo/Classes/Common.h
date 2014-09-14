9bdf680365eec40712bfc17386e7915dd1a9c4bc
bennya8 <benny_a8@live.com>
2014-09-14 15:36:47 +0800

    add common header

---
+++ ios_HomebrewWeibo/Classes/Common.h
@@ -0,0 +1,9 @@
+#import "UIImage+Extend.h"
+
+
+#define kAppFont @"HelveticaNeue-Light"
+
+#define kWeiboSDKAppKey         @"2308818352"
+#define kWeiboSDKAppSecrect     @"0cd8a8f3abc289cb0ab2b1479fb4c45e"
+#define kWeiboSDKRedirectURI    @"http://www.sina.com"
+
