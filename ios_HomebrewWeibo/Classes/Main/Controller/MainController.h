9f05a392eeae0990be992fe4f3d1d7298dd295cd
bennya8 <benny_a8@live.com>
2014-09-14 15:38:52 +0800

    add MainModule

---
+++ ios_HomebrewWeibo/Classes/Main/Controller/MainController.h
@@ -0,0 +1,13 @@
+//
+//  MainController.h
+//  ios_HomebrewWeibo
+//
+//  Created by Benny on 9/14/14.
+//  Copyright (c) 2014 Benny. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MainController : UITabBarController
+
+@end
