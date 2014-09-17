2917e5027b0feeaa77a6af09afe9da8233c613ec
bennya8 <benny_a8@live.com>
2014-09-17 18:52:49 +0800

    add new macro

--- ios_HomebrewWeibo/Classes/Common.h
+++ ios_HomebrewWeibo/Classes/Common.h
@@ -2,8 +2,14 @@
 
 
 #define kAppFont @"HelveticaNeue-Light"
+#define kAppFontNormal @"HelveticaNeue"
+#define kAppFontBold @"HelveticaNeue-Bold"
+
 
 #define kWeiboSDKAppKey         @"2308818352"
 #define kWeiboSDKAppSecrect     @"0cd8a8f3abc289cb0ab2b1479fb4c45e"
 #define kWeiboSDKRedirectURI    @"http://www.sina.com"
+
+
+#define kAppColorLightBlue [UIColor colorWithRed:0.114 green:0.431 blue:0.945 alpha:1]
 
