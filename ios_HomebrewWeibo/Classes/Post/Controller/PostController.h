2f0e7dd0990da1dbd3ab7833b562b63097ce1058
bennya8 <benny_a8@live.com>
2014-09-17 18:57:45 +0800

    add Post Controller

---
+++ ios_HomebrewWeibo/Classes/Post/Controller/PostController.h
@@ -0,0 +1,13 @@
+//
+//  PostController.h
+//  ios_HomebrewWeibo
+//
+//  Created by Benny on 9/15/14.
+//  Copyright (c) 2014 Benny. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PostController : UIViewController
+
+@end
